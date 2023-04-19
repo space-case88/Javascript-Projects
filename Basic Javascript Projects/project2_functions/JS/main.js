be11a3d0e598bcec8f2fdc14732db6a6cb855855
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-04-19 13:17:26 -0700

    Add files via upload

---
+++ Basic Javascript Projects/project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+var button = document.getElementById("myButton"); //this is a reference to the button element
+
+button.addEventListener("click", function() {   // Add an event listener for the "click" event
+    console.log("Button clicked!");     // Do something when the button is clicked
+  });
+
+function myFunction() {         // This is the function that coincides with the p element
+    var sentence = "I am learning"; 
+    sentence += " a lot from AI!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
