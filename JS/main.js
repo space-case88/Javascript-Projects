aa4b26d5f354a060f685bd3c1ca8ec405bb94de1
space-case88 <128758348+space-case88@users.noreply.github.com>
2023-04-19 11:32:19 -0700

    Add files via upload
    This is updated with expressions, sent1 and sent2 strings getting concatenated.

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+var Family = "The Arenzzinis", Dad = "Jeremiah", Mom = "Hermione", Daughter = "Penny", Son = "Zorro";
+
+document.write(Dad); //This is calling out the variable Dad
+
+document.write(21 + 21) //This is adding up to the answer to life, the universe and everything
+
+window.alert("This is my window alert")
+
+var Sent1 = "This is the beginning of the string"; //This is the sent1 and sent2 Variables
+var Sent2 = " and this is the end of the string";
+
+var concatenatedString = Sent1 + Sent2; //sent1 and sent2 concatenated
+console.log(concatenatedString);
+
+document.write(concatenatedString) //this should display the variables on the page
+
+var sum = 2+3; //This is my expression
+
+document.write(sum) //my expression written out
